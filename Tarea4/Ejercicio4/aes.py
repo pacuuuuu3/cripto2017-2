68fad1a5a9fc863a46c3c5ef426248ec232f13ef
Victor <victorzamoragutierrez@gmail.com>
2017-04-25 11:45:07 -0500

    Desencripta cbc

--- Tarea4/Ejercicio4/aes.py
+++ Tarea4/Ejercicio4/aes.py
@@ -30,18 +30,41 @@ def encripta_aes_cbc(mensaje, llave, iv):
     i = 0 # Contador
     criptotexto = b'' # Texto cifrado que vamos a regresar
     entrada = iv # Entrada encadenada de la función
+    cifrador = AES.new(llave, AES.MODE_ECB) # Cifrador AES
     while i < len(mensaje):
         bloque = mensaje[i:i+16] #Bloque a cifrar en AES
         a_cifrar = bxor(entrada, bloque) # XOR de las entradas
-        cifrador = AES.new(llave, AES.MODE_ECB) # Cifrador AES
         cifrado = cifrador.encrypt(a_cifrar) # Bloque cifrado
         criptotexto += cifrado        
         entrada = cifrado
         i+=16
     return iv + criptotexto
 
+'''
+Descifra un mensaje encriptado en CBC
+mensaje - El mensaje a descifrar
+llave - La llave con la cual descifraremos
+'''
+def descifra_aes_cbc(mensaje, llave):
+    mensaje_claro = b'' # El mensaje claro
+    iv = mensaje[0:16] # Sacamos el vector de inicialización
+    mensaje = mensaje[16:]
+    i = 0 # Contador
+    descifrador = AES.new(llave, AES.MODE_ECB) # Objeto para descifrar  
+    anterior = iv # Variable para descifrar en cadena
+    while i < len(mensaje):
+        bloque = mensaje[i:i+16] # Bloque a descifrar
+        bloque_descifrado = descifrador.decrypt(bloque) # Desciframos el bloque
+        bloque_claro = bxor(bloque_descifrado, anterior) # XOR de las entradas
+        mensaje_claro += bloque_claro
+        anterior = bloque
+        i += 16
+    return mensaje_claro
+         
+        
 iv = os.urandom(16) # Vector de inicialización
 mensaje = b'hola'
 llave = b'Sixteen byte key'
-print(encripta_aes_cbc(mensaje,llave, iv))
+print(encripta_aes_cbc(mensaje,llave, iv)) 
 print(iv + AES.new(llave, AES.MODE_CBC, iv).encrypt(padding(mensaje)))
+print(descifra_aes_cbc(encripta_aes_cbc(mensaje, llave, iv), llave))
