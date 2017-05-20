f40fc3d55b0d6b5acc56b7c743c1e2f5d53c40b8
Luis P. Mayo <pmayov@gmail.com>
2017-05-19 23:21:27 -0500

    Ejercicio 8 listo

--- Tarea5/ejercicio8.py
+++ Tarea5/ejercicio8.py
@@ -12,8 +12,6 @@ pk1 = RSA.importKey(k1)
 n1 = pk1.n # modulo del campo
 e1 = pk1.e # exponente de la llave pública
 
-# parte privada
-d1 = inverse_mod(e1,n1) 
 
 ### Segunda llave
 k2 = """-----BEGIN PUBLIC KEY-----
@@ -23,8 +21,7 @@ nTj1X/8Hq7pLYNIVE57Yxoyr3zTOOBJufgTNzdKS0Rc5Ti4zZUkCkQvpAgMBAAE=
 
 pk2 = RSA.importKey(k2)
 n2 = pk2.n
-e2 = pk2.e)
-d2 = inverse_mod(e2,n2)
+e2 = pk2.e
 
 p = gcd(n1,n2)
 q1 = n1/p
@@ -33,12 +30,15 @@ print("el factor en común es {0} \n su cofactor en la primera llave es {1} \n s
 
 
 ### Para descifrar el mensaje
-# tup = (n,e,d)
-# privk = RSA.construct(tup)
+phi_n = (p-1)*(q1-1)
+d1 = inverse_mod(e1,phi_n)
+type(d1)
+tup = (long(n1),long(e1),long(d1))
+privk = RSA.construct(tup)
 
 # mensaje cifrado 
-# c = 0x41b4e1609390ff8fb5f225b010d1cc79253dcab1744d5f865daabad0e28d259141722382114d9a73106b4d429676dae60a1528a0eb3b73eab0e9d2165c72492f 
-# m = privk.decrypt(c)
-# print("el mensaje es {0}".format(m))
+c = 0x41b4e1609390ff8fb5f225b010d1cc79253dcab1744d5f865daabad0e28d259141722382114d9a73106b4d429676dae60a1528a0eb3b73eab0e9d2165c72492f 
+m = privk.decrypt(c)
+print("el mensaje es {0}".format(m))
 
 
